3f86d05167323c7c3c5088f47f14b90951ace38a
wuguodong <18610639412@163.com>
2017-03-17 15:46:22 +0800

    高德地图sdk  定位！

--- app/src/main/java/com/wii/study/lession_11/MapActivity.java
+++ app/src/main/java/com/wii/study/lession_11/MapActivity.java
@@ -1,21 +1,40 @@
 package com.wii.study.lession_11;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.DPoint;
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.CoordinateConverter;
 import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.UiSettings;
@@ -40,10 +59,19 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
     private OnLocationChangedListener mListener = null;
     //标识，用于判断是否只显示一次定位信息和用户重新定位
     private boolean isFirstLoc = true;
+
+    private static final String TAG = MapActivity.class.getSimpleName();
+    private double latitude = 0.0;
+    private double longitude = 0.0;
+
+    private LocationManager locationManager;
+    TextView info;
+    private  AMapLocation aMapLocationGPS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        info = (TextView) findViewById(R.id.tvLat);
         //获取地图控件引用
         mapView = (MapView) findViewById(R.id.map_view);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
@@ -57,20 +85,64 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
             settings.setMyLocationButtonEnabled(true);
             aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
         }
-        //定位的小图标 默认是蓝点 这里自定义一团火，其实就是一张图片
         /**
          *
          *
-        MyLocationStyle myLocationStyle = new MyLocationStyle();
-        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.drawable.location));
-        myLocationStyle.radiusFillColor(android.R.color.transparent);
-        myLocationStyle.strokeColor(android.R.color.transparent);
-        aMap.setMyLocationStyle(myLocationStyle);
-        Log.i("当前APK使用的签名:",sHA1.sHA1(getBaseContext()));
-        **/
+         MyLocationStyle myLocationStyle = new MyLocationStyle();
+         myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.drawable.location));
+         myLocationStyle.radiusFillColor(android.R.color.transparent);
+         myLocationStyle.strokeColor(android.R.color.transparent);
+         aMap.setMyLocationStyle(myLocationStyle);
+         Log.i("当前APK使用的签名:",sHA1.sHA1(getBaseContext()));
+         **/
         //开始定位
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            getLocation();
+        } else {
+            toggleGPS();
+            new Handler() {
+            }.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getLocation();
+                }
+            }, 2000);
+        }
         location();
+
     }
+
+    private void toggleGPS() {
+        Intent gpsIntent = new Intent();
+        gpsIntent.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+        gpsIntent.addCategory("android.intent.category.ALTERNATIVE");
+        gpsIntent.setData(Uri.parse("custom:3"));
+        try {
+            PendingIntent.getBroadcast(this, 0, gpsIntent, 0).send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (location1 != null) {
+                latitude = location1.getLatitude(); // 经度
+                longitude = location1.getLongitude(); // 纬度
+            }
+        }
+
+    }
+
     private void location() {
         //初始化定位
         mLocationClient = new AMapLocationClient(getApplicationContext());
@@ -82,6 +154,7 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
         mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
         //设置是否返回地址信息（默认返回地址信息）
         mLocationOption.setNeedAddress(true);
+        mLocationOption.setGpsFirst(true);
         //设置是否只定位一次,默认为false
         mLocationOption.setOnceLocation(false);
         //设置是否强制刷新WIFI，默认为强制刷新
@@ -103,18 +176,21 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
         mLocationClient.stopLocation();//停止定位
         mLocationClient.onDestroy();//销毁定位客户端。
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
         mapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
         mapView.onPause();
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -134,58 +210,59 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
 
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
-        if (aMapLocation != null) {
-            if (aMapLocation.getErrorCode() == 0) {
-                //定位成功回调信息，设置相关消息
-                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
-                aMapLocation.getLatitude();//获取纬度
-                aMapLocation.getLongitude();//获取经度
-                aMapLocation.getAccuracy();//获取精度信息
-                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                Date date = new Date(aMapLocation.getTime());
-                df.format(date);//定位时间
-                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
-                aMapLocation.getCountry();//国家信息
-                aMapLocation.getProvince();//省信息
-                aMapLocation.getCity();//城市信息
-                aMapLocation.getDistrict();//城区信息
-                aMapLocation.getStreet();//街道信息
-                aMapLocation.getStreetNum();//街道门牌号信息
-                aMapLocation.getCityCode();//城市编码
-                aMapLocation.getAdCode();//地区编码
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    //定位成功回调信息，设置相关消息
+                    aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
+                    aMapLocation.getLatitude();//获取纬度
+                    aMapLocation.getLongitude();//获取经度
+                    aMapLocation.getAccuracy();//获取精度信息
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date(aMapLocation.getTime());
+                    df.format(date);//定位时间
+                    aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                    aMapLocation.getCountry();//国家信息
+                    aMapLocation.getProvince();//省信息
+                    aMapLocation.getCity();//城市信息
+                    aMapLocation.getDistrict();//城区信息
+                    aMapLocation.getStreet();//街道信息
+                    aMapLocation.getStreetNum();//街道门牌号信息
+                    aMapLocation.getCityCode();//城市编码
+                    aMapLocation.getAdCode();//地区编码
 
-                // 如果不设置标志位，此时再拖动地图时，它会不断将地图移动到当前的位置
-                if (isFirstLoc) {
-                    //设置缩放级别
-                    aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
-                    //将地图移动到定位点
-                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
-                    //点击定位按钮 能够将地图的中心移动到定位点
-                    mListener.onLocationChanged(aMapLocation);
-                    //添加图钉
-                    aMap.addMarker(getMarkerOptions(aMapLocation));
-                    //获取定位信息
-                    StringBuffer buffer = new StringBuffer();
-                    buffer.append(aMapLocation.getCountry() + ""
-                            + aMapLocation.getProvince() + ""
-                            + aMapLocation.getCity() + ""
-                            + aMapLocation.getProvince() + ""
-                            + aMapLocation.getDistrict() + ""
-                            + aMapLocation.getStreet() + ""
-                            + aMapLocation.getStreetNum());
-                    Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
-                    isFirstLoc = false;
+                    // 如果不设置标志位，此时再拖动地图时，它会不断将地图移动到当前的位置
+                    if (isFirstLoc) {
+                        //设置缩放级别
+                        aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+                        //将地图移动到定位点
+                        aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                        //点击定位按钮 能够将地图的中心移动到定位点
+                        mListener.onLocationChanged(aMapLocation);
+                        //添加图钉
+                        aMap.addMarker(getMarkerOptions(aMapLocation));
+                        //获取定位信息
+                        StringBuffer buffer = new StringBuffer();
+                        buffer.append(aMapLocation.getCountry() + ""
+                                + aMapLocation.getProvince() + ""
+                                + aMapLocation.getCity() + ""
+                                + aMapLocation.getProvince() + ""
+                                + aMapLocation.getDistrict() + ""
+                                + aMapLocation.getStreet() + ""
+                                + aMapLocation.getStreetNum());
+                        Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
+                        isFirstLoc = false;
+                    }
+
+
+                } else {
+                    //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError", "location Error, ErrCode:"
+                            + aMapLocation.getErrorCode() + ", errInfo:"
+                            + aMapLocation.getErrorInfo());
+                    Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
                 }
-
-
-            } else {
-                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
-                Log.e("AmapError", "location Error, ErrCode:"
-                        + aMapLocation.getErrorCode() + ", errInfo:"
-                        + aMapLocation.getErrorInfo());
-                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
             }
-        }
+
     }
 
     //自定义一个图钉，并且设置图标，当我们点击图钉时，显示设置的信息
@@ -197,7 +274,7 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
         //位置
         options.position(new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude()));
         StringBuffer buffer = new StringBuffer();
-        buffer.append(amapLocation.getCountry() + "" + amapLocation.getProvince() + "" + amapLocation.getCity() +  "" + amapLocation.getDistrict() + "" + amapLocation.getStreet() + "" + amapLocation.getStreetNum());
+        buffer.append(amapLocation.getCountry() + "" + amapLocation.getProvince() + "" + amapLocation.getCity() + "" + amapLocation.getDistrict() + "" + amapLocation.getStreet() + "" + amapLocation.getStreetNum());
         //标题
         options.title(buffer.toString());
         //子标题
@@ -208,4 +285,91 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
         return options;
 
     }
+
+
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        } else {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+        }
+        info.setText("纬度：" + latitude + "\n" + "经度：" + longitude);
+    }
+    LocationListener locationListener = new LocationListener() {
+        // Provider的状态在可用、暂时不可用和无服务三个状态直接切换时触发此函数
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        // Provider被enable时触发此函数，比如GPS被打开
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(TAG, provider);
+        }
+
+        // Provider被disable时触发此函数，比如GPS被关闭
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(TAG, provider);
+        }
+
+        // 当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                Log.e("Map", "Location changed : Lat: " + location.getLatitude() + " Lng: " + location.getLongitude());
+                latitude = location.getLatitude(); // 经度
+                longitude = location.getLongitude(); // 纬度
+
+                aMapLocationGPS= fromGpsToAmap(location);
+            }
+        }
+    };
+
+    /**
+     * 检测当的网络（WLAN、3G/2G）状态
+     * @param context Context
+     * @return true 表示网络可用
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null && info.isConnected())
+            {
+                // 当前网络是连接的
+                if (info.getState() == NetworkInfo.State.CONNECTED)
+                {
+                    // 当前所连接的网络可用
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public AMapLocation fromGpsToAmap(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        latLng = CoordinateUtil.transformFromWGSToGCJ(latLng);
+        AMapLocation aMapLocation = new AMapLocation(location);
+        aMapLocation.setLatitude(latLng.latitude);
+        aMapLocation.setLongitude(latLng.longitude);
+
+        return aMapLocation;
+    }
 }
+
